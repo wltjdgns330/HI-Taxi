93a8aa1544c11ebc2ce2e126bbd08820805d6d71
lhj3529 <hyun3529@naver.com>
2016-09-25 05:38:13 +0900

    5시 38분

--- app/src/main/java/com/example/sunghoon/hitaxi_new/SignupActivity.java
+++ app/src/main/java/com/example/sunghoon/hitaxi_new/SignupActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignupActivity extends Activity {
 
@@ -19,6 +21,7 @@ public class SignupActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
         final Spinner spi = (Spinner) findViewById(R.id.signup_spi);
         String[] str = getResources().getStringArray(R.array.spinnerArray);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
@@ -43,30 +46,34 @@ public class SignupActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignupActivity.this,MainTabActivity.class);
-
-               /* EditText ed1 = (EditText)findViewById(R.id.signup_name);
+                Intent intent = new Intent(SignupActivity.this, MainTabActivity.class);
+                EditText ed1 = (EditText) findViewById(R.id.signup_name);
                 Name = ed1.getText().toString();
-                EditText ed2 = (EditText)findViewById(R.id.signup_email);
-                Email = ed2.getText().toString();
-                RadioGroup ra = (RadioGroup)findViewById(R.id.rr);
 
+                /*RadioGroup ra = (RadioGroup) findViewById(R.id.rr);
                 int aa = ra.getCheckedRadioButtonId();
-                RadioButton rb = (RadioButton)findViewById(aa);
-                Gender = rb.getText().toString();
+                RadioButton rb = (RadioButton)ra.findViewById(aa);
+                Gender = rb.getText().toString();*/
 
-                TextView tv = (TextView)findViewById(R.id.tvv);
-                tv.append("Name : " + Name + " Em : " + Email  + Em + " Gender : " + Gender);*/
+                EditText ed2 = (EditText) findViewById(R.id.signup_email);
+                Email = ed2.getText().toString();
 
-                startActivity(intent);
-                finish();
-
-
+                //TextView tv = (TextView)findViewById(R.id.tvv);
+                //tv.append("Name : " + Name + " Em : " + Email  + Em + " Gender : " + Gender);
+                if(!Name.equals("") && !Email.equals(""))//rb.getText()!=null &&
+                {
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignupActivity.this, "빈칸을 채워주세요", Toast.LENGTH_LONG).show();
+                    //intent = new Intent(SignupActivity.this, SignupActivity.class);
+                    //finish();
+                }
             }
         });
 
-
     }
-
 
 }
