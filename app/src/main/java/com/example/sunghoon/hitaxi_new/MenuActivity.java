0a7cadf6684be489a49db2fd0f1aec6d99a6efd7
lhj3529 <hyun3529@naver.com>
2016-09-24 11:41:53 +0900

    메뉴 다시

---
+++ app/src/main/java/com/example/sunghoon/hitaxi_new/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.sunghoon.hitaxi_new;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by 이현주 on 2016-09-24.
+ */
+public class MenuActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
