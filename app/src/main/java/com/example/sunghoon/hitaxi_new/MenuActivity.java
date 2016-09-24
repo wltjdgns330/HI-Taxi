897d1478b77a7db2c04e94abce5ec9253d6d5c6e
lhj3529 <hyun3529@naver.com>
2016-09-24 18:08:10 +0900

    dialog 만듦

--- app/src/main/java/com/example/sunghoon/hitaxi_new/MenuActivity.java
+++ app/src/main/java/com/example/sunghoon/hitaxi_new/MenuActivity.java
@@ -1,9 +1,14 @@
 package com.example.sunghoon.hitaxi_new;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -15,12 +20,55 @@ public class MenuActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Button btn1 = (Button) findViewById(R.id.bnt_make);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        Button bt = (Button)findViewById(R.id.bnt_make);
+        bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                mOnClick(view);
             }
         });
+    }
+
+    public void mOnClick(View v)
+    {
+
+        switch(v.getId()){
+            case R.id.bnt_make:
+                LayoutInflater inflater = getLayoutInflater();
+
+                final View dialogView = inflater.inflate(R.layout.dialog_makeroom, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("방 만들기");
+                builder.setView(dialogView);
+                builder.setPositiveButton("방만들기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        //EditText edit_name = (EditText)dialogView.findViewById(R.id.room_title);
+                        //RadioGroup rg = (RadioGroup)dialogView.findViewById(R.id.dialog_rg);
+
+                        //String name = edit_name.getText().toString();
+                        //int checkedId = rg.getCheckedRadioButtonId();
+
+
+                        Log.i("t1","tt");
+                        //RadioButton rb = (RadioButton)rg.findViewById(checkedId);
+                        //String number = rb.getText().toString();
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(MenuActivity.this, "방 만들기를 취소합니다", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.setCanceledOnTouchOutside(false);
+
+                dialog.show();
+                break;
+        }
     }
 }
